5fb1442465f2ebb9a67f1cb37810b8dda03cd9c9
Radosav Perisic <radosavperisic00@gmail.com>
2023-10-17 15:29:42 +0200

    Initial MongoDB Setup and Connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://radosavperisiccontact:izZftdf0qL6OeIJI@game-catalog.kvimp4f.mongodb.net/?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose'
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Hello world!')
+})
+
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+console.log('App connected to database');
 app.listen(PORT, () => {
     console.log(`App is listening to :${PORT}`);
+});
+
+}).catch((error)=>{
+console.log(error);
 });
