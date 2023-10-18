d8a285858af5bcb5920dee208ed9200dfa9280b5
Radosav Perisic <radosavperisic00@gmail.com>
2023-10-18 13:47:40 +0200

    Refactor App Component for Routing

--- frontend/game-catalog-frontend/src/App.jsx
+++ frontend/game-catalog-frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import DeleteGame from './pages/DeleteGame'
+import EditGame from './pages/EditGame'
+import ShowGame from './pages/ShowGame'
+import CreateGame from './pages/CreateGame'
 
 const App = () => {
   return (
-    <div className='bg-gray-600 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/games/create' element={<CreateGame/>}/>
+      <Route path='/games/details/:id' element={<ShowGame/>}/>
+      <Route path='/games/edit/:id' element={<EditGame/>}/>
+      <Route path='/games/delete/:id' element={<DeleteGame/>}/>
+    </Routes>
   )
 }
 
