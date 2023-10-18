e65f51c79aa512af75dde25fb2fe7f3baa4f2ebd
Radosav Perisic <radosavperisic00@gmail.com>
2023-10-18 18:24:01 +0200

    Integrate Axios for API Data Retrieval

--- frontend/game-catalog-frontend/src/pages/Home.jsx
+++ frontend/game-catalog-frontend/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const [games, setGames] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(()=>{
+        setLoading(true)
+        axios.get('http://localhost:5555/games').then((response) => {
+            setGames(response.data.data);
+            setLoading(false);
+        }).catch((error) => {
+            console.log(error);
+            setLoading(false)
+        })
+    },[])
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
